73284ae26095ae0c3d08aa080ca06bcd212b2b60
Augusto F. Hack <hack.augusto@gmail.com>
2017-06-17 22:44:51 -0300

    removed ls wrapper

--- .zsh/commands.sh
+++ .zsh/commands.sh
@@ -1,35 +1,3 @@
-bin() {
-    command -v $1 >/dev/null 2>&1
-}
-
-LS_BIN=$(bin ls)
-
-# Make -B overwritable by -a/-A
-ls(){
-    show_all=false
-    i=1
-    for arg in $@; do
-        if [[ $arg == '-B' ]]; then
-            argv[$i]=()
-        elif [[ $arg == '-a' ]]; then
-            show_all=true
-            argv[$i]=()
-        elif [[ $arg == '-A' ]]; then
-            show_all=true
-            argv[$i]=()
-        fi
-        ((i=i+1))
-    done
-
-    if ($show_all); then
-        show_all='-A'
-    else
-        show_all='-B'
-    fi
-
-    $LS_BIN --classify --color=auto $show_all $argv
-}
-
 if (( ! $+commands[ack] && $+commands[ack-grep] )); then
     alias ack='ack-grep';
 fi
