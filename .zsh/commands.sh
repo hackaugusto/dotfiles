7e123f57d16d159fd4420f52e7d358ac737a1a25
Augusto F. Hack <hack.augusto@gmail.com>
2019-04-10 06:26:34 -0300

    fixed diff return code

--- .zsh/commands.sh
+++ .zsh/commands.sh
@@ -6,14 +6,17 @@ if (( $+commands[wdiff] )); then
     if (( $+commands[cwdiff] )); then
         diff(){
             /usr/bin/wdiff $@ | awk '/\[-/,/-\]/; /{\+/,/\+}/' | cwdiff -f
+            return $pipestatus[1]
         }
     else
         diff(){
             /usr/bin/wdiff $@ | awk '/\[-/,/-\]/; /{\+/,/\+}/'
+            return $pipestatus[1]
         }
     fi
 elif (( $+commands[colordiff] && $+commands[diff] )); then
     diff(){
         /usr/bin/diff $@ | colordiff
+        return $pipestatus[1]
     }
 fi
