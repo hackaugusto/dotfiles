472db4637a1ea68871d1976e31dc882001998c26
Augusto F. Hack <me@hak.cx>
2022-03-10 16:08:36 +0100

    zsh: updated plugins

--- .zsh/plugins.sh
+++ .zsh/plugins.sh
@@ -11,41 +11,20 @@ if ! zgen saved; then
     zgen load zsh-users/zsh-syntax-highlighting
 
     # utilities
-    zgen load StackExchange/blackbox
-    zgen load Tarrasch/zsh-autoenv
-    zgen oh-my-zsh plugins/extract
-    zgen load zdharma/zsh-diff-so-fancy
-    # zgen oh-my-zsh plugins/gitfast
+    # zgen load zdharma-continuum/zsh-diff-so-fancy
 
-    # web
-    # zgen oh-my-zsh plugins/bower
-    # zgen oh-my-zsh plugins/grunt
-    # zgen oh-my-zsh plugins/httpie
-
-    # python
-    zgen oh-my-zsh plugins/autopep8
-    zgen oh-my-zsh plugins/celery
-    zgen oh-my-zsh plugins/fabric
+    # python (not available in zsh-completions as of 10/Mar/22)
     zgen oh-my-zsh plugins/pep8
     zgen oh-my-zsh plugins/pip
-    zgen oh-my-zsh plugins/python
-    # zgen oh-my-zsh plugins/paver
-    # zgen oh-my-zsh plugins/pylint
-    # zgen oh-my-zsh plugins/django
+    zgen oh-my-zsh plugins/pipenv
+    zgen oh-my-zsh plugins/pylint
 
     # other languages
     zgen load lukechilds/zsh-nvm
-    zgen load rust-lang/zsh-config
+    # zgen oh-my-zsh plugins/rust
     zgen oh-my-zsh plugins/adb
     zgen oh-my-zsh plugins/docker
     zgen oh-my-zsh plugins/docker-compose
-    # zgen oh-my-zsh plugins/scala
-    # zgen oh-my-zsh plugins/sbt
-    # zgen oh-my-zsh plugins/ant
-    # zgen oh-my-zsh plugins/lein
-    # zgen oh-my-zsh plugins/brew
-    # zgen oh-my-zsh plugins/capistrano
-    # zgen oh-my-zsh plugins/gradle
 
     zgen save
 fi
