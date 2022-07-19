55e386df331482f6759841ad18ace55350e70a6f
Augusto Hack <hacka@Augustos-MacBook-Pro.local>
2022-07-19 10:29:03 +0200

    ext no longer necessary

--- firefox/github-ext/github.js
+++
@@ -1,9 +0,0 @@
-function full_head_ref(){
-    // Instead of showing <user>:<branch> it shows <user>/<repo>:<branch>
-    //
-    // This can then be used to add the user as a remote
-    el = document.querySelector('.gh-header-meta .head-ref');
-    el.textContent = el.title;
-}
-
-full_head_ref();
