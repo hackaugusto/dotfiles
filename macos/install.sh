ebda458c6775c1cd28d497ee882ff0194f1f6bdd
Augusto Hack <hack@Augustos-MacBook-Air.fritz.box>
2022-09-20 19:03:38 +0200

    mac: added gpg dependencies

--- macos/install.sh
+++ macos/install.sh
@@ -1,4 +1,6 @@
+#!/usr/bin/env sh
+
 # Install brew.sh
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-brew install jq tmux semgrep
+brew install jq tmux semgrep pinentry-mac gpg
