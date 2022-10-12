dc9773b22682db1e59ddfd7fcbbe402a7b5c71b7
Augusto F. Hack <hack.augusto@gmail.com>
2022-10-12 19:36:49 +0200

    mac: added some fonts

--- macos/install.sh
+++ macos/install.sh
@@ -3,6 +3,11 @@
 # Install brew.sh
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-brew install jq tmux semgrep pinentry-mac gpg tig iterm2 showkey
+
+# svn is required for font-source-code-pro
+brew install jq tmux semgrep pinentry-mac gpg tig iterm2 showkey svn
 
 brew install --cask --no-quarantine middleclick alacritty
+
+brew tap homebrew/cask-fonts
+brew install font-inconsolata font-source-code-pro
